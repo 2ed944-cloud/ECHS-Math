175dad32ee9dcb7850d24186bfdfddf51736b5f7
2ed944-cloud <2ed944@qla.qfschools.qa>
2026-07-27 14:42:40 +0300

    Refresh cache for the extrema reveal hero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "echs-platform-school-control-v3-pathway2-landing1-builder1-hybrid1";
+const VERSION = "echs-platform-school-control-v3-pathway2-landing1-builder1-hybrid2";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const SHELL = [
